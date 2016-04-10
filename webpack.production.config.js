c3ee2a3ba0b1764e842957ed70bda6ce09fe2373
Brandon <brandilla1337@gmail.com>
2016-04-10 04:11:19 -0500

    added resolve for webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -32,6 +32,9 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
+  resolve: {
+  	extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [
       {
